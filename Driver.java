cbf06fb7cd352937663dca531af5e48fabd4bcbc
TiffanyCao <tcao00@stuy.edu>
2018-11-07 14:41:39 -0800

    tested constructor and toString

---
+++ Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+  public static void main(String[] args){
+    WordSearch first = new WordSearch(4, 5);
+
+    System.out.println("---Testing Constructor---");
+    System.out.println("testing toString: should print a 4 by 5 empty word search");
+    System.out.println(first.toString());
+    
+  }
+}
