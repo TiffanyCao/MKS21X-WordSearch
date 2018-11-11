63a0528c030732c8ce7cf84497a919a1e666ceda
TiffanyCao <tcao00@stuy.edu>
2018-11-10 18:35:56 -0800

    updated addWord method

--- WordSearch.java
+++ WordSearch.java
@@ -45,8 +45,8 @@ public class WordSearch{
       File f = new File(filename);
       Scanner in = new Scanner(f);
       while(in.hasNext()){
-        String word = in.next();
-        wordsToAdd.add(word);
+        String word = in.nextLine();
+        wordsToAdd.add(word.toUpperCase());
       }
     }
 
@@ -80,6 +80,70 @@ public class WordSearch{
       } result += "(seed: " + this.seed + ")";
       return result;
     }
+
+
+     /**Attempts to add a given word to the specified position of the WordGrid.
+      *The word is added in the direction rowIncrement,colIncrement
+      *Words must have a corresponding letter to match any letters that it overlaps.
+      *
+      *@param word is any text to be added to the word grid.
+      *@param row is the vertical locaiton of where you want the word to start.
+      *@param col is the horizontal location of where you want the word to start.
+      *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
+      *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
+      *@return true when: the word is added successfully.
+      *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
+      *        OR there are overlapping letters that do not match
+      */
+    private boolean addWord(String word, int row, int col, int rowIncrement, int colIncrement){
+      if(row < 0 || row >= data.length){
+        return false;
+      }
+      if(col < 0 || col >= data[0].length){
+        return false;
+      }
+      if(rowIncrement == 0 && colIncrement == 0){
+        return false;
+      }
+      if(rowIncrement == 0){
+        for(int x = 0; x < word.length(); x++){
+          if((data[row][(colIncrement * x) + col] != '_') &&
+             (data[row][(colIncrement * x) + col] != word.charAt(x))){
+            return false;
+          }
+        } for(int x = 0; x < word.length(); x++){
+          data[row][(colIncrement * x) + col] = word.charAt(x);
+        } wordsAdded.add(word);
+        return true;
+      }
+      if(colIncrement == 0){
+        for(int x = 0; x < word.length(); x++){
+          if((data[(rowIncrement * x) + row][col] != '_') &&
+             (data[(rowIncrement * x) + row][col] != word.charAt(x))){
+            return false;
+          }
+        } for(int x = 0; x < word.length(); x++){
+          data[(rowIncrement * x) + row][col] = word.charAt(x);
+        } wordsAdded.add(word);
+        return true;
+      }
+      for(int x = 0; x < word.length(); x++){
+        if((data[(rowIncrement * x) + row][(colIncrement * x) + col] != '_') &&
+           (data[(rowIncrement * x) + row][(colIncrement * x) + col] != word.charAt(x))){
+          return false;
+        }
+      } for(int x = 0; x < word.length(); x++){
+        data[(rowIncrement * x) + row][(colIncrement * x) + col] = word.charAt(x);
+      } wordsAdded.add(word);
+      return true;
+    }
+
+      /*[rowIncrement,colIncrement] examples:
+      *[-1,1] would add up and the right because (row -1 each time, col + 1 each time)
+      *[ 1,0] would add downwards because (row+1), with no col change
+      *[ 0,-1] would add towards the left because (col - 1), with no row change
+      */
+
 
 
     /**Attempts to add a given word to the specified position of the WordGrid.
